20a1b00738996fad55d20ba9b04471623876433a
AleksGin <alexanderginin@icloud.com>
2024-12-02 11:14:57 +0300

    create a load, save, add, remove methods in Library class

--- app/library/library.py
+++ app/library/library.py
@@ -1,3 +1,34 @@
+import json
+from utils.pharses import Phrases
+
+
 class Library:
-    def __init__(self) -> None:
-        ...
+    def __init__(self, filename="library.json") -> None:
+        self.filename = filename
+        self.books = self.load_books()
+
+    def load_books(self):
+        try:
+            with open(self.filename, "r", encoding="utf-8") as file:
+                return json.load(file)
+        except Exception:
+            return []
+
+    def save_book(self):
+        with open(self.filename, "w", encoding="utf-8") as file:
+            json.dump(self.books, file, indent=4, ensure_ascii=False)
+
+    def add_book(self, author: str):
+        self.books.append(author)
+        self.save_book()
+
+    def remove_books(self, title):
+        book_to_remove = next(
+            (book for book in self.books if book["title"].lower() == title.lower()),
+            None,
+        )
+        if book_to_remove:
+            self.books.remove(book_to_remove)
+            self.save_book()
+            return Phrases.successful_removal_text.format(title)
+        return Phrases.book_not_found.format(title)

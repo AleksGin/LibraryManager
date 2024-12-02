c11c065d79947936394bf5c67b095ef30a2dda8d
AleksGin <alexanderginin@icloud.com>
2024-12-02 10:49:38 +0300

    add test methods to class User

--- app/user/user.py
+++ app/user/user.py
@@ -1,4 +1,30 @@
+from library import Library
+from services import LibraryService
+from utils.pharses import MainMenu
+
+
 class User:
-    def __init__(self, library_service: ...) -> None:
-        pass
-    
+    def __init__(self, library_service: LibraryService) -> None:
+        self.library_service = library_service
+
+    def add_book(self):
+        author = input("Введите название автора: ")
+        message = self.library_service.add_book(author=author)
+        print(message)
+
+
+def main_menu():
+    library = Library()
+    library_service = LibraryService(library=library)
+    user = User(library_service=library_service)
+
+    while True:
+        print(MainMenu.menu_text)
+        command = input("> ")
+
+        if command == "1":
+            user.add_book()
+
+
+if __name__ == "__main__":
+    main_menu()

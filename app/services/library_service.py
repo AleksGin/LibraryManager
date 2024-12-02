edd0bb5fa840dba4dd08ad0f3397ab0628fae0eb
AleksGin <alexanderginin@icloud.com>
2024-12-02 10:49:55 +0300

    add add_book method to LibraryService

--- app/services/library_service.py
+++ app/services/library_service.py
@@ -1,4 +1,11 @@
+from library import Library
+from utils.pharses import Phrases
+
+
 class LibraryService:
-    def __init__(self, library: ...) -> None:
-        
-        
+    def __init__(self, library: Library) -> None:
+        self.library = library
+
+    def add_book(self, author: str):
+        self.library.add_book(author=author)
+        return Phrases.successful_add_test.format(author)

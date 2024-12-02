b323c03df9abefe4384718ff9903058f13c9cd56
AleksGin <alexanderginin@icloud.com>
2024-12-03 00:55:04 +0300

    refactor add_book in LibraryService and add method _check_for_duplicate

--- app/services/library_service.py
+++ app/services/library_service.py
@@ -6,6 +6,11 @@ class LibraryService:
     def __init__(self, library: Library) -> None:
         self.library = library
 
-    def add_book(self, author: str):
-        self.library.add_book(author=author)
-        return Phrases.successful_add_test.format(author)
+    def add_book(self, details: dict):
+        self.library.add_book(details=details)
+        print(Phrases.successful_add_test.format())
+        
+    @staticmethod
+    def _check_for_duplicate(library: Library):
+        ...
+    

b206656e739e40816f669fe143f0ea2f6ccc2629
AleksGin <alexanderginin@icloud.com>
2024-12-02 14:05:17 +0300

    added new phrases to Phrases class

--- app/utils/pharses/phrases.py
+++ app/utils/pharses/phrases.py
@@ -1,4 +1,8 @@
 class Phrases:
-    successful_removal_text = "Книга <<{}>> была удалена"
+    successful_removal = "Книга <<{}>> была удалена"
     book_not_found = "Книга <<{}>> не найдена :("
     successful_add_test = "Автор: {} успешно добавлен!"
+    ask_for_author = "Введите имя автора: "
+    ask_for_title = "Введите название книги: "
+    ask_for_year = "Введите год издания: "
+    

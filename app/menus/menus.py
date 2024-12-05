766a32435b73dccd2a98fae101df635d1db13e69
AleksGin <alexanderginin@icloud.com>
2024-12-05 17:13:01 +0300

    add space symbol to menu

--- app/menus/menus.py
+++ app/menus/menus.py
@@ -1,21 +1,23 @@
 from commands import Command
-from utils.pharses import ErrorPhrases
+from utils.pharses import (
+    ErrorPhrases,
+    Phrases,
+)
 
 
-class Menus:
+class Menu:
     def __init__(
         self,
         options: dict[str, tuple[str, Command]],
-        exit_phrase: str,
     ) -> None:
         self.options = options
-        self.exit_phrase = exit_phrase
 
     def show_menu(self):
         while True:
+            print("\n")
             for key, (description, _) in self.options.items():
                 print(f"{key}. {description}")
-            command = input("> ")
+            command = input(f"{Phrases.choice_command}> ")
 
             if command in self.options:
                 _, action = self.options[command]
